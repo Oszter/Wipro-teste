4d4697a6784946fbf8d4a2c30fbebb9a8a77c2c1
Rodrigo Gallego Oszter <70962043+Oszter@users.noreply.github.com>
2021-07-16 17:01:27 -0300

    Update BancoTesteQuestoes.sql

--- BancoTesteQuestoes.sql
+++ BancoTesteQuestoes.sql
@@ -1,40 +1,42 @@
 --1
 SELECT 
-	s.dsStatus AS status, 
-	COUNT(P.idProcesso) AS qtd 
+      s.dsStatus AS status, 
+      COUNT(P.idProcesso) AS qtd 
 FROM 
-	tb_Processo p 
-	INNER JOIN tb_Status s ON s.idStatus = p.idStatus 
+      tb_Processo p 
+      INNER JOIN tb_Status s ON s.idStatus = p.idStatus 
 WHERE 
-	p.idStatus IN (SELECT idStatus FROM tb_Status) 
-	GROUP BY 
-		s.dsStatus
+      p.idStatus IN (SELECT idStatus FROM tb_Status) 
+GROUP BY 
+      s.dsStatus
 
 		
 --2 
 SELECT
-    idProcesso
+      idProcesso
 FROM 
-    tb_Andamento 
+      tb_Andamento 
 WHERE YEAR
-    (dtAndamento) = '2013' 
+      (dtAndamento) = '2013' 
 ORDER BY 
-    dtAndamento DESC
+      dtAndamento 
+DESC
 
 
 --3
-SELECT t1.* 
+SELECT 
+      t1.* 
 FROM (
-	SELECT 
-		DISTINCT COUNT(p.dtEncerramento) AS qtd, 
-		p.dtEncerramento AS Data 
-	FROM 
-		tb_Processo p 
-	GROUP BY 
-		p.DtEncerramento
-	) AS t1 
-	WHERE 
-		t1.qtd >= 5
+     SELECT 
+	     DISTINCT COUNT(p.dtEncerramento) AS qtd, 
+	     p.dtEncerramento AS Data 
+     FROM 
+	     tb_Processo p 
+     GROUP BY 
+	     p.DtEncerramento
+     ) AS t1 
+     WHERE 
+	     t1.qtd >= 5
 
 --4
 SELECT REPLICATE
